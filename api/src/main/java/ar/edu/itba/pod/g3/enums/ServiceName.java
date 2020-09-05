052980cbc03203e9b6f43c8afa27131b282d7870
nachito <igvidaurreta@gmail.com>
2020-09-05 20:25:38 -0300

    Implement managementClient

---
+++ api/src/main/java/ar/edu/itba/pod/g3/enums/ServiceName.java
@@ -0,0 +1,19 @@
+package ar.edu.itba.pod.g3.enums;
+
+/**
+ * enum that determines which remote service to call from the Server.
+ */
+public enum ServiceName {
+    MANAGEMENT ("managementService"),
+    VOTE ("voteService"),
+    QUERY("queryService");
+
+    final String name;
+    ServiceName(String name){
+        this.name = name;
+    }
+
+    public String getServiceName(){
+        return name;
+    }
+}
--- client/src/main/java/ar/edu/itba/pod/g3/client/Client.java
+++ client/src/main/java/ar/edu/itba/pod/g3/client/Client.java
@@ -1,12 +1,24 @@
 package ar.edu.itba.pod.g3.client;
 
+import ar.edu.itba.pod.g3.client.interfaces.ManagementClient;
+import ar.edu.itba.pod.g3.enums.ServiceName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Client {
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Properties;
+
+public abstract class Client {
     private static Logger logger = LoggerFactory.getLogger(Client.class);
 
-    public static void main(String[] args) {
-        logger.info("election-system Client Starting ...");
+    static Remote getRemoteService(final String address, final ServiceName serviceName) throws NotBoundException, RemoteException{
+        logger.info(String.format("Connecting to %s:%d/%s", address, 1099, serviceName.getServiceName()));
+        final Registry registry = LocateRegistry.getRegistry(address, 1099);
+        // return registry.lookup(serviceName.getServiceName());
+        return null;
     }
 }
