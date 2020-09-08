7f511676f34dea86aefd79375dc6c21247889198
nachito <igvidaurreta@gmail.com>
2020-09-08 19:57:37 -0300

    Move interface to API and finish functionality

--- client/src/main/java/ar/edu/itba/pod/g3/client/VotingClient.java
+++ client/src/main/java/ar/edu/itba/pod/g3/client/VotingClient.java
@@ -1,13 +1,19 @@
 package ar.edu.itba.pod.g3.client;
 
 import ar.edu.itba.pod.g3.client.utils.VoteParser;
+import ar.edu.itba.pod.g3.interfaces.VotingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.rmi.RemoteException;
 import java.util.Properties;
 
 import static ar.edu.itba.pod.g3.enums.ServiceName.VOTE;
 
+/**
+ * Client that runs the voting client.
+ * Used to emit votes
+ */
 public class VotingClient extends Client{
     private static Logger logger = LoggerFactory.getLogger(VotingClient.class);
     public static void main(String[] str){
@@ -15,7 +21,7 @@ public class VotingClient extends Client{
         Properties properties = System.getProperties();
         try {
             if(containsValidArguments(properties)){
-                VotingClient client = (VotingClient) getRemoteService(properties.getProperty("serverAddress"), VOTE);
+                VotingService client = (VotingService) getRemoteService(properties.getProperty("serverAddress"), VOTE);
                 executeAction(client, properties.getProperty("votesPath"));
             }else {
                 System.out.println("Not found");
@@ -30,8 +36,13 @@ public class VotingClient extends Client{
         return Client.containsValidArguments(properties) && properties.containsKey("votesPath");
     }
 
-    private static void executeAction(VotingClient client, String votePath){
+    private static void executeAction(VotingService client, String votePath){
         VoteParser parser = new VoteParser(votePath);
         parser.parseVotes();
+        try {
+            client.submitVotes(parser.getParsedVotes());
+        }catch (RemoteException ex){
+            ex.printStackTrace();
+        }
     }
 }
