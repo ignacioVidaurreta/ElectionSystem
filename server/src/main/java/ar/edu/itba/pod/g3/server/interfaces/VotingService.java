01d71a6c3faa1bc651a30ee99e6d356407cd6fd8
frandelgado <fdelgado@itba.edu.ar>
2020-09-09 20:15:16 -0300

    emitVote now supports a collection of votes

--- server/src/main/java/ar/edu/itba/pod/g3/server/interfaces/VotingService.java
+++ server/src/main/java/ar/edu/itba/pod/g3/server/interfaces/VotingService.java
@@ -2,6 +2,7 @@ package ar.edu.itba.pod.g3.server.interfaces;
 
 import ar.edu.itba.pod.g3.models.Vote;
 
+import java.util.Collection;
 public interface VotingService {
-    boolean emitVote(Vote vote) throws Exception;
+    boolean emitVote(Collection<Vote> vote) throws Exception;
 }
