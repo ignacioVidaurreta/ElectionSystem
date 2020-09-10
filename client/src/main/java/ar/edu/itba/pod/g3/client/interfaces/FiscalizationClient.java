e23bf61e2bbf63d95da4e82ab538eea97b202d80
nachito <igvidaurreta@gmail.com>
2020-09-10 20:56:29 -0300

    remove unused interface FiscalizationClient

--- client/src/main/java/ar/edu/itba/pod/g3/client/interfaces/FiscalizationClient.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.itba.pod.g3.client.interfaces;
-
-import ar.edu.itba.pod.g3.api.models.Fiscal;
-
-public interface FiscalizationClient {
-    boolean registerFiscal(Fiscal fiscal);
-
-}
