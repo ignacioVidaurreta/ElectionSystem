fbf5b0ce33d780ea08acd6c714e0e4c3efc5979b
nachito <igvidaurreta@gmail.com>
2020-09-17 01:11:40 -0300

    remove obsolete commentary + formatting

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-votes_path="/tmp/rand_votes.csv"
-
-
-
+VOTES_PATH="/tmp/rand_votes.csv"
 
 function run_gazzilion_votes(){
   REPS=${1:-30}
   echo $REPS
   for i in $(seq $REPS); do
     generate_random_file 3000
-    ./run-vote.sh -DvotesPath="$votes_path" -DserverAddress="localhost" >> out.log&
+    ./run-vote.sh -DvotesPath="$VOTES_PATH" -DserverAddress="localhost" >> out.log&
   done
 }
 
@@ -22,7 +19,7 @@ function run_concurrent_test() {
     else
       echo "Hello"
       generate_random_file 3000
-      ./run-vote.sh -DvotesPath="$votes_path" -DserverAddress="localhost" >> out.log&
+      ./run-vote.sh -DvotesPath="$VOTES_PATH" -DserverAddress="localhost" >> out.log&
     fi
   done
   sleep 10
@@ -31,26 +28,18 @@ function run_concurrent_test() {
 }
 
 function generate_random_file(){
-  rm -rf $votes_path
+  rm -rf $VOTES_PATH
   booths=(1000 1001 1002 1003 1004 1005)
   parties=("TIGER" "JACKALOPE" "LEOPARD" "LYNX") # Only using a few parties, not all.
   provinces=("JUNGLE" "SAVANNAH" "TUNDRA")
 
-  # The first line overwrites what was written before
   for i in $(seq "$1"); do
-    echo "${booths[$((RANDOM % 6))]};${provinces[$((RANDOM % 3))]};TIGER|$((1 + RANDOM % 5)),LEOPARD|$((1 + RANDOM % 5)),JACKALOPE|$((1 + RANDOM % 5)),LYNX|$((1 + RANDOM % 5));${parties[$((RANDOM % 4))]}" >> $votes_path
+    echo "${booths[$((RANDOM % 6))]};${provinces[$((RANDOM % 3))]};TIGER|$((1 + RANDOM % 5)),LEOPARD|$((1 + RANDOM % 5)),JACKALOPE|$((1 + RANDOM % 5)),LYNX|$((1 + RANDOM % 5));${parties[$((RANDOM % 4))]}" >> $VOTES_PATH
   done
   
 }
 
-
-function set_up_testing(){
-  rm -rf out.log
-  generate_random_file 100
-}
-
 function main(){
-  set_up_testing
   command="$1"
   shift
   case "$command" in
