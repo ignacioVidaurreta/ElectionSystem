638cf2d078d2e6b67600da164562d64464be4f54
nachito <igvidaurreta@gmail.com>
2020-09-17 18:31:05 -0300

    fix usage of run query

--- scripts/run-query.sh
+++ scripts/run-query.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 if [ $# -lt 2 ]; then
-    echo "Invalid arguments; Usage: ./run-vote -DserverAddress=<ip> -DoutPath=<filename> [ -Dstate=<stateName> | -Did=<pollingPlaceNumber> ]"
+    echo "Invalid arguments; Usage: ./run-query -DserverAddress=<ip> -DoutPath=<filename> [ -Dstate=<stateName> | -Did=<pollingPlaceNumber> ]"
     exit 1
 fi
 
 cd ../client/target/election-system-client-1.0-SNAPSHOT/ || (echo "Error!"; exit 1)
 ./run-query.sh "$@"
+
